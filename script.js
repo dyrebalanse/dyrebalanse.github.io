b4c67eebce30329e6caca4149ae49426e5ce742f
dyrebalanse <mariusdalby84@gmail.com>
2026-03-05 11:08:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let cart = [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
 
 function addToCart(name, price){
 
 cart.push({name, price});
 
+localStorage.setItem("cart", JSON.stringify(cart));
+
 alert(name + " lagt i handlekurv");
 
 updateCart();
+
 }
+
 
 function updateCart(){
 
 let total = 0;
 
 cart.forEach(item => total += item.price);
-
-document.getElementById("cartTotal").innerText =
-"Handlekurv: " + cart.length + " varer | " + total + " kr";
-
-}
